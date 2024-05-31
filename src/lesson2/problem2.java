3024118646bb77a16480aaa71f8b25f740d6c440
hpauyu <hpauyunanshawng@yahoo.com>
2024-05-30 20:15:13 -0500

    reformat code

--- src/lesson2/problem2.java
+++ src/lesson2/problem2.java
@@ -5,8 +5,8 @@ public class problem2 {
         float a = 1.27f;
         float b = 3.881f;
         float c = 9.6f;
-        float sum = a+b+c;
-        System.out.println((int)sum);
+        float sum = a + b + c;
+        System.out.println((int) sum);
         System.out.println(Math.round(sum));
     }
 }
--- src/lesson2/problem6.java
+++ src/lesson2/problem6.java
@@ -2,21 +2,22 @@ package lesson2;
 
 public class problem6 {
     public static void main(String[] args) {
-        secondMin(new int[]{2,21,3,45,0,12,18,6,3,1,0,22});
+        secondMin(new int[]{2, 21, 3, 45, 0, 12, 18, 6, 3, 1, 0, 22});
     }
-    public static void secondMin(int[] arrayOfInts){
+
+    public static void secondMin(int[] arrayOfInts) {
         int smallest = Integer.MAX_VALUE;
         int secondOne = Integer.MAX_VALUE;
-        for (int i : arrayOfInts){
-            if ( i < smallest){
+        for (int i : arrayOfInts) {
+            if (i < smallest) {
                 smallest = i;
             }
         }
-        for (int i :arrayOfInts){
-            if ( i == smallest){
+        for (int i : arrayOfInts) {
+            if (i == smallest) {
                 continue;
             }
-            if (i < secondOne){
+            if (i < secondOne) {
                 secondOne = i;
             }
         }
