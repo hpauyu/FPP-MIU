a7637b97ca7ca46a299f4f93b10497741ff4f513
hpauyu <hpauyunanshawng@yahoo.com>
2024-06-14 17:09:19 -0500

    lab9 added

---
+++ src/lab9/Prob1/src/prob1/Admin.java
@@ -0,0 +1,8 @@
+package lab9.Prob1.src.prob1;
+import java.util.*;
+public class Admin {
+	public static HashMap<Key, Student> processStudents(List<Student> students) {
+		//implement
+		return null;
+	}
+}
---
+++ src/lab9/Prob1/src/prob1/Standing.java
@@ -0,0 +1,5 @@
+package lab9.Prob1.src.prob1;
+
+public enum Standing {
+	FRESHMAN, SOPHOMORE, JUNIOR, SENIOR;
+}
